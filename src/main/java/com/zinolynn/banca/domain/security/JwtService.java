43189eb99128d1b98774369a4833b71dbb738794
Osuolale <mohd@yahoo.com>
2025-08-22 12:35:00 +0100

    jwt builder changed

--- src/main/java/com/zinolynn/banca/domain/security/JwtService.java
+++ src/main/java/com/zinolynn/banca/domain/security/JwtService.java
@@ -3,23 +3,28 @@ package com.zinolynn.banca.domain.security;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
 @Service
 public class JwtService {
-    private final String secretKey = "your_secret_key"; // put this in config later
+    private final SecretKey secretKey = Keys.hmacShaKeyFor(
+            "a2FhU29tZVN1cGVyU2VjcmV0S2V5MTIzNDU2Nzg5MA==".getBytes(StandardCharsets.UTF_8)
+    );// put this in config later
 
     public String generateToken(UserDetails userDetails) {
         return Jwts.builder()
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date())
                 .setExpiration(Date.from(Instant.now().plus(1, ChronoUnit.DAYS)))
-                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .signWith(secretKey, SignatureAlgorithm.HS256)  // ✅ proper key usage
                 .compact();
     }
 
