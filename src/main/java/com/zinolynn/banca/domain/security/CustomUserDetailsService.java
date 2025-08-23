5e7d50aa2bfea084d31a86ff3f92b1ffdbd4417f
Osuolale <mohd@yahoo.com>
2025-08-23 22:12:51 +0100

    Added ROLE_USER to authorization

--- src/main/java/com/zinolynn/banca/domain/security/CustomUserDetailsService.java
+++ src/main/java/com/zinolynn/banca/domain/security/CustomUserDetailsService.java
@@ -4,11 +4,13 @@ import com.zinolynn.banca.domain.entities.User;
 import com.zinolynn.banca.domain.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 
 
@@ -25,7 +27,7 @@ public class CustomUserDetailsService implements UserDetailsService {
         return new org.springframework.security.core.userdetails.User(
                 user.getEmail(),
                 user.getPassword(),
-                List.of() // no roles for now
+                Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"))
         );
     }
 }
