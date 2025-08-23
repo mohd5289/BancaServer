de8b30fa1f7a68e5cafa3dc7900baf73e118c2eb
Osuolale <mohd@yahoo.com>
2025-08-23 15:12:06 +0100

    Disable security

--- src/main/java/com/zinolynn/banca/domain/security/SecurityConfig.java
+++ src/main/java/com/zinolynn/banca/domain/security/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authenticationProvider(authenticationProvider())
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+//                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
