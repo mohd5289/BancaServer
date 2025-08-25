a47ee4297a1c546f9b8120b2f592a2765e4cb527
Osuolale <mohd@yahoo.com>
2025-08-25 21:08:48 +0100

    Change stripes' return type

--- src/main/java/com/zinolynn/banca/domain/controllers/StripeController.java
+++ src/main/java/com/zinolynn/banca/domain/controllers/StripeController.java
@@ -1,9 +1,14 @@
 package com.zinolynn.banca.domain.controllers;
 
 
+import com.stripe.model.Account;
 import com.zinolynn.banca.domain.services.StripeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/stripe")
@@ -14,7 +19,23 @@ public class StripeController {
 
     @PostMapping("/account")
     public Object createAccount(@RequestParam String email, @RequestParam String country) throws Exception {
-        return stripeService.createConnectedAccount(email, country);
+        Map<String, Object> params = new HashMap<>();
+        params.put("type", "custom");
+        params.put("country", country);
+        params.put("email", email);
+        params.put("business_type", "individual");
+
+        Account account = Account.create(params);
+
+        // Build safe response for client
+        Map<String, Object> response = new HashMap<>();
+        response.put("id", account.getId());
+        response.put("email", account.getEmail());
+        response.put("country", account.getCountry());
+        response.put("charges_enabled", account.getChargesEnabled());
+        response.put("payouts_enabled", account.getPayoutsEnabled());
+
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping("/payment-intent")
